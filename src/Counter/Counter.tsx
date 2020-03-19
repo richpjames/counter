8b7ffd487a427da707dd7f36a63022991b0c6aba
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-19 17:21:22 +0000

    add button components to counter

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Button from "./Button";
 
 function Counter() {
-  return <div>0</div>;
+  return (
+    <section>
+      0<Button textToDisplay="+" label="increment" />
+      <Button textToDisplay="-" label="decrement" />
+    </section>
+  );
 }
 
 export default Counter;
