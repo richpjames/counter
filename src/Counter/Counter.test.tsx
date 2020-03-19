6b9d7587995d7628dabf07fdb3f8d997951b5ca8
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-19 17:36:19 +0000

    refactor second test to be more descriptive and add increment test

--- src/Counter/Counter.test.tsx
+++ src/Counter/Counter.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import React from "react";
 import Counter from "./Counter";
 import "@testing-library/jest-dom/extend-expect";
@@ -9,6 +9,20 @@ test("renders the number 0", () => {
 });
 test("renders a '+' and '-' button", () => {
   const { getByText } = render(<Counter />);
+
   const incrementButton = getByText("+");
-  const minusButton = getByText("-");
+  const decerementButton = getByText("-");
+  expect(incrementButton).toBeInTheDocument();
+  expect(decerementButton).toBeInTheDocument();
+});
+
+test("pressing the + button increments the counter by 1", () => {
+  const { getByText, getByTitle } = render(<Counter />);
+
+  const counter = getByTitle("counter");
+  const incrementButton = getByText("+");
+
+  expect(counter).toHaveTextContent("0");
+  fireEvent.click(incrementButton);
+  expect(counter).toHaveTextContent("1");
 });
