80cdccc563f35a3578270a4f4a7dab3d574e0f27
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-19 18:05:45 +0000

    add +3 incrementation functionality

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -4,14 +4,24 @@ import Button from "./Button";
 function Counter() {
   const [count, setCount] = useState(0);
 
+  const incrementCount = (amountToIncrement: number) => {
+    if (count < 10) {
+      setCount(prevCount => prevCount + amountToIncrement);
+    }
+  };
+
   return (
     <section>
       <h2 title="counter">{count}</h2>
-      <Button textToDisplay="+3" label="increment by 3" />
+      <Button
+        textToDisplay="+3"
+        label="increment by 3"
+        onClick={() => incrementCount(3)}
+      />
       <Button
         textToDisplay="+"
         label="increment"
-        onClick={() => count < 10 && setCount(prevCount => prevCount + 1)}
+        onClick={() => incrementCount(1)}
       />
       <Button
         textToDisplay="-"
