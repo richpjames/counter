625daaf901d43aa7fd009362132241fc95875b02
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-20 10:10:50 +0000

    style buttons and upper bound

--- src/Counter/Counter-styles.tsx
+++ src/Counter/Counter-styles.tsx
@@ -7,9 +7,22 @@ export const CounterContainer = styled.section`
   width: 50%;
   color: #ffffff;
 `;
+export const Count = styled.h2`
+  font-family: "Bungee Inline", sans-serif;
+`;
 
 export const ButtonsContainer = styled.section`
   display: flex;
+  margin-top: 20px;
+  height: 100px;
 `;
-
-export const UpperBoundInput = styled.input``;
+export const UpperBoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  align-items: center;
+`;
+export const UpperBoundInput = styled.input`
+  width: 40px;
+  margin-top: 7.5px;
+`;
