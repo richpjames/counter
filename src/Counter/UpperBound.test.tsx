46b07aa0b9d0326f390a9fc11fa3de854403945c
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-20 11:54:51 +0000

    check upperBound onChange function works

--- src/Counter/UpperBound.test.tsx
+++ src/Counter/UpperBound.test.tsx
@@ -12,3 +12,15 @@ test("renders the number passed to it", () => {
   const upperBoundInput = getByLabelText(/Counter Limit:/);
   expect(upperBoundInput).toHaveValue(243);
 });
+
+test("the onChange function returns the number passed into it", () => {
+  const mockFunc = jest.fn();
+
+  const { getByLabelText } = render(
+    <UpperBound upperBound={0} setUpperBound={mockFunc} />
+  );
+  const counterLimit = getByLabelText(/Counter Limit:/);
+
+  userEvent.type(counterLimit, "1");
+  expect(mockFunc).toHaveBeenCalledWith(1);
+});
