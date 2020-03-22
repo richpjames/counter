ac27942b1b11c3c73a622247f2c67a2c1faabfd5
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-22 19:06:03 +0000

    add help icon and message

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./Counter/Counter";
 import styled from "styled-components/macro";
+import { CircleQuestion } from "grommet-icons";
+import Dialog from "./Dialog";
 
 const AppContainer = styled.div`
   display: flex;
   justify-content: center;
   height: 100vh;
+  width: 100vw;
+  position: absolute;
 `;
-
+const HelpIcon = styled.nav`
+  position: absolute;
+  top: 15px;
+  left: 15px;
+`;
 function App() {
+  const [showHelp, setShowHelp] = useState(false);
+  const hideDialog = () => setShowHelp(() => false);
   return (
-    <AppContainer>
-      <Counter />
-    </AppContainer>
+    <>
+      <AppContainer>
+        <HelpIcon onClick={() => setShowHelp(true)}>
+          <CircleQuestion color="#FFFFFF" />
+        </HelpIcon>
+        <Counter />
+      </AppContainer>
+      {showHelp && (
+        <Dialog
+          hide={hideDialog}
+          confirm={hideDialog}
+          confirmText="OK"
+          title="Info"
+          text="This app was created for demo purposes by Rich James. It is not endorsed or produced by ao.com"
+        />
+      )}
+    </>
   );
 }
 
---
+++ src/Dialog.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styled from "styled-components/macro";
+import Button from "./Button";
+
+const DialogWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid black;
+  opacity: 1;
+  width: 60%;
+  height: 40%;
+  border-radius: 8px;
+  opacity: 1;
+  background-color: #ffffff;
+`;
+const Layer = styled.section`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  z-index: 5;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Heading = styled.h3`
+  margin-top: 5%;
+  margin-bottom: 2.5%;
+  text-align: center;
+`;
+
+const Text = styled.p`
+  text-align: center;
+  width: 80%;
+`;
+
+const ButtonsBox = styled.div`
+  margin-top: 25px;
+  margin-right: 150px;
+  margin-bottom: 25px;
+  margin-left: 150px;
+`;
+
+interface Props {
+  title: string;
+  text?: string;
+  confirm: () => void;
+  confirmText: string;
+  hide: () => void;
+}
+
+function Dialog(props: Props) {
+  const { title, text, confirm, confirmText, hide } = props;
+  return (
+    <Layer>
+      <DialogWrapper>
+        <Heading>{title}</Heading>
+        <Text>{text}</Text>
+        <ButtonsBox>
+          <Button label="Ok" onClick={() => confirm()} disabled={false}>
+            {confirmText}
+          </Button>
+        </ButtonsBox>
+      </DialogWrapper>
+    </Layer>
+  );
+}
+
+export default Dialog;
