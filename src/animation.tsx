456fded0c3551125c00582af5fd43a34367be2cc
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-23 10:15:05 +0000

    extract animations to seperate file

---
+++ src/animation.tsx
@@ -0,0 +1,11 @@
+import { keyframes } from "styled-components";
+
+export const letterDropDown = keyframes`
+0% {transform: translateY(-40%);}
+100% {transform: translateY(0);}
+`;
+
+export const smileDropDown = keyframes`
+0% {transform: translateY(-90%);}
+100% {transform: translateY(0);}
+`;
